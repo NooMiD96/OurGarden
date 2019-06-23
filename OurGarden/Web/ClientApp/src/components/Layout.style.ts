43fb7b8a3e969061f2a371071e8e9165a97bf942
Дмитрий <koshak-9696@inbox.ru>
2019-06-23 21:13:21 +0300

    style

---
+++ OurGarden/Web/ClientApp/src/components/Layout.style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export default styled.div`
+  height: 100%;
+
+  .ant-layout {
+    height: 100%;
+  }
+`;
--- OurGarden/Web/ClientApp/src/components/Layout.tsx
+++ OurGarden/Web/ClientApp/src/components/Layout.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react';
 
 import AntdLayout from '@core/antd/Layout';
-// import { Text, Paragraph } from "@core/antd/Typography";
-// import { ConfigProvider, Empty } from "antd";
 
 import ErrorHandler from '@core/HOC/ErrorHandler';
 import NavMenu from './NavMenu';
 
+import LayoutWrapper from "./Layout.style";
+
 const { Header, Content, Footer } = AntdLayout;
-// const customizeRenderEmpty = () => (
-//   <Empty
-//     description={<Text>Данных нет</Text>}
-//   />
-// );
 
 export const Layout = ({
   children
@@ -20,28 +15,19 @@ export const Layout = ({
   children?: React.ReactNode;
 }) => (
   <ErrorHandler>
-    <AntdLayout>
-      <Header className="antd-header">
-        <NavMenu />
-      </Header>
-      <Content className="main-content-wrapper">
-        {/* <ConfigProvider renderEmpty={customizeRenderEmpty}> */}
-        {children}
-        {/* </ConfigProvider> */}
-      </Content>
-      <Footer className="footer">
-        {/* <Paragraph>
-          Адрес: г. Тула, ул. Эн, 0
-        </Paragraph>
-        <Paragraph>
-          Телефон: 8 (4872) 00-00-00
-        </Paragraph>
-        <Paragraph>
-          E-mail: med.sister@gmail.com
-        </Paragraph> */}
-        © 2019
-      </Footer>
-    </AntdLayout>
+    <LayoutWrapper>
+      <AntdLayout>
+        <Header className="antd-header">
+          <NavMenu />
+        </Header>
+        <Content className="main-content-wrapper">
+          {children}
+        </Content>
+        <Footer className="footer">
+          © 2019
+        </Footer>
+      </AntdLayout>
+    </LayoutWrapper>
     <div id="global-modals-container" />
   </ErrorHandler>
 )
