366e8a835e8f39e0f040b116a2681306fa02bf9f
Nickitina Anastasya <nikitina@cmsplanet.ru>
2019-07-13 20:06:09 +0300

    хелпер для алиаса

---
+++ Core/Helpers/StringHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Core.Helpers
+{
+    public static class StringHelper
+    {
+        public static string Transform(string text, int maxLength = 255)
+        {
+            text = RemoveTags(text.ToLower());
+            return text.Substring(0, Math.Min(text.Length, maxLength));
+        }
+
+        private static string RemoveTags(string str)
+        {
+            //string result = Regex.Replace(str, @"(?></?\w+)(?>(?:[^>'""]+|'[^']*'|""[^""]*"")*)>", String.Empty);
+            string result = Regex.Replace(str, @"[.,\/#!$%\^&\*;:{}=\/_`~()""]", String.Empty);
+            result = Regex.Replace(result, "&nbsp;", " ");
+            result = Regex.Replace(result, "&mdash;|&ndash;", "-");
+            result = Regex.Replace(result.Trim(), " ", "-");
+
+            return result;
+        }
+    }
+}
