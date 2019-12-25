398282b624444a0845265e269f68e1a098ae9ca0
Дмитрий <koshak-9696@inbox.ru>
2019-12-25 20:27:01 +0300

    Adaptive

--- OurGarden/Web/ClientApp/webpack/assetsModuleRules.ts
+++ OurGarden/Web/ClientApp/webpack/assetsModuleRules.ts
@@ -62,7 +62,17 @@ const getAssetsModuleRules = (
   },
   {
     test: /\.svg$/,
-    use: "react-svg-loader"
+    use: [
+      {
+        loader: "babel-loader"
+      },
+      {
+        loader: "react-svg-loader",
+        options: {
+          jsx: true // true outputs JSX tags
+        }
+      }
+    ]
   },
   // https://webpack.js.org/loaders/url-loader/
   // https://webpack.js.org/loaders/file-loader/
